335c6e3513e2a621e17350a72f926fded7ef48a0
obetsa <obetsa@gmail.com>
2021-01-29 11:15:54 +0200

    hw1

---
+++ 1.py
@@ -0,0 +1,26 @@
+1
+var_int = 25
+var_float = 4.5
+var_str = 'python'
+
+2
+var_big = var_int * 1.5
+
+3
+var_float = var_float - 1
+
+4
+result =  var_big % var_float
+print (result)
+
+5
+var_str = "end"
+
+6
+print (var_int, var_float, var_big, var_str)
+
+7
+number = int(input("введите число :"))
+result = number // 10 + number % 10
+print ("Число:", result)
+
---
+++ hi.py
@@ -0,0 +1,4 @@
+print('hi dude!')
+print (2 * 15, type (2 * 442))
+print (10 / 0)
+print ('asd') 
