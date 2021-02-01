d0d6da4c2674f29496ab0a712b6d9eddceadbf89
obetsa <obetsa@gmail.com>
2021-02-01 17:13:14 +0200

    hw2

--- hw2/calc.py
+++ hw2/calc.py
@@ -6,8 +6,26 @@
     Результат выводится на экран.
     * обработать все возможные ошибки программы с помощью try-except
 """
-
-number_1 = int(input())
-number_2 = int(input())
-
-print(number_1 + number_2)
+try:
+    number_1 = int(input("Введіть перше число: "))
+    operation = input('''Введіть символ:
++ додавання
+- віднімання
+* множення
+/ ділення
+''')
+    number_2 = int(input("Введіть друге число: "))
+    if operation == "+":
+        print(number_1 + number_2)
+    elif operation == "-":
+        print(number_1 - number_2)
+    elif operation == "*":
+        print(number_1 * number_2)
+    elif operation == "/":
+        print(number_1 / number_2)
+    else:
+        print("Введені дані не коректні")
+except ValueError:
+    print("Введено не коректні дані")
+except ZeroDivisionError:
+    print("Ділення на 0 неможливе")
--- hw2/quarter.py
+++ hw2/quarter.py
@@ -11,3 +11,19 @@
             III |    IV
                 |
 """
+
+x = int(input("x = "))
+y = int(input("y = "))
+
+if x > 0 and y > 0:
+    print("1")
+elif x > 0 and y < 0:
+    print("2")
+elif x < 0 and y < 0:
+    print("3")
+elif x < 0 and y > 0:
+    print("4")
+elif x == 0 or y == 0:
+    print("Точка в центрі або на осі")
+else:
+    print("Координати введено невірно")
