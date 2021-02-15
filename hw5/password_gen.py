0e71ea2be73e53b82390d16a0cc3843498e19fa2
obetsa <obetsa@gmail.com>
2021-02-15 15:01:31 +0200

    password_gen_complete

--- hw5/password_gen.py
+++ hw5/password_gen.py
@@ -19,9 +19,65 @@
         - * игнорируются пробелы
 """  # noqa: E501
 
+import string
+import random
+
 
 def main():
-    pass
+    print('''Виберіть пароль:
+1 - легкий
+2 - середній
+3 - тяжкий
+4 - пароль користувача''')
+    choise_pass = int(input(''))
+    if choise_pass == 1:
+        l_password()
+    elif choise_pass == 2:
+        m_password()
+    elif choise_pass == 3:
+        s_password()
+    elif choise_pass == 4:
+        user_password()
+    else:
+        print('Невірні дані, спробуйти ще раз')
+    return
+
+
+def l_password():
+    size = 8
+    lowercase = (string.ascii_lowercase)
+    light_p = "".join(random.sample(lowercase, size))
+    print(light_p)
+
+
+def m_password():
+    size = 8
+    lowercase = (string.ascii_lowercase)
+    uppercase = (string.ascii_uppercase)
+    number = (string.digits)
+    middle = uppercase + lowercase + number
+    middle_p = "".join(random.sample(middle, size))
+    print(middle_p)
+
+
+def s_password():
+    size = random.randint(8, 16)
+    lowercase = (string.ascii_lowercase)
+    uppercase = (string.ascii_uppercase)
+    number = (string.digits)
+    symbols = (string.punctuation)
+    strong = uppercase + lowercase + number + symbols
+    strong_p = ''.join(random.sample(strong, size))
+    print(strong_p)
+
+
+def user_password():
+    sumbols = input('Введіть символи з яких буде генеруватися пароль: ')
+    size_p = int(input('Введіть довжину пароля: '))
+    if size_p <= 8:
+        print('Пароль не надійний')
+    u_pass = "".join(random.sample(sumbols, size_p))
+    print(u_pass)
 
 
 if __name__ == '__main__':
