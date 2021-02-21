1e67587e8e86dcbe092e66637883689152341a6e
obetsa <obetsa@gmail.com>
2021-02-21 15:06:05 +0200

    phone_book

--- hw6/phone_book.py
+++ hw6/phone_book.py
@@ -13,37 +13,36 @@
 import re
 
 
-def phone():
+def read_book():
+    accounts = []
     with open("files/phone_book.txt", "r") as f:
         lines = f.readlines()
-        matches = []
         for line in lines:
-            if re.search(r"w+(a|A)\b|(m|M)\w+\b", line):
-                matches += line
-        for match in matches:
-            name = (re.findall(r'\w+', match))
-            # number = re.search(r'\d+')
-            print(name)
-
-# re.match(r"([m|M])\w+\b", line)    
-# or re.match(r"([aA])\w+", line):
-# re.findall(r"[a-zA-Z]+", string)     
-
-
-# def written():
-#     with open("files/phone_book.txt", "w") as f:
-            
+            if re.search(r"\w+(a|A)\b|(m|M)\w+\b", line):
+                pname = re.search(r"\w+(a|A)\b|(m|M)\w+\b", line)
+                name = pname.group()
+                print(name)
+                pdigit = re.findall(r"\d+", line)
+                digit = "".join(pdigit)
+                print(digit)
+                accounts.append((digit, name))
+    return accounts
 
 
+def save_book(accounts):
+    with open("files/edited_phone_book.txt", "a") as f:
+        i = 1
+        for account in accounts:
+            print(f"{i} {account[0]} - {account[1]}\n")
+            f.write(f"{i} {account[0]} - {account[1]}\n")
+            i += 1
 
 
 def main():
-    phone()
-#     name = get_name()
-#     surname = input("+Enter your surname: ")
-#     age = input("Enter your age: ")
+    accounts = read_book()
+    print(accounts)
+    save_book(accounts)
 
-#     save_info(name, surname, age)
 
 if __name__ == "__main__":
     main()
