f0c042c7a83c8b0bb310a8868aa47242aa35df62
obetsa <obetsa@gmail.com>
2021-03-02 13:19:04 +0200

    slow dec

--- hw9/slow_decorator.py
+++ hw9/slow_decorator.py
@@ -11,3 +11,29 @@
     Используйте библиотеку time, а именно функции time и sleep
 
 """
+
+import time
+
+
+def time_decorator(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        execution_time = time.time() - start
+        print(f"\nExecution time {func.__name__} {execution_time} seconds")
+
+        if execution_time > 5:
+            print(f"{func.__name__} - very slow function")
+
+        return result
+
+    return wrapper
+
+
+def slow_decorator(func):
+    def wrapper(*args, **kwargs):
+        time.sleep(5)
+        return func(*args, **kwargs)
+
+    return
+
